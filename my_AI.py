d04509bf33e0de9b46e865122cd0aba36d80c0dc
Nabil Hajchaib <21168@ecam.be>
2023-03-20 11:05:41 +0100

    mon premier code client

--- my_AI.py
+++ my_AI.py
@@ -1,5 +1,17 @@
-lez gooooooooo
-input("tangente de pi/2")
-return input 
-hello world for i in true while x>7:
-  return "ayoub"
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host, port = ('localhost', 4000)
+
+try :
+    s.connect((host, port))
+    print("Le client se connecte... ")
+
+    data = ' J peux moi aussi ouuuu que les petits ? T as les kramptés ? Hein ? Inpainyain, Quoicoubeh, quoicoubeh, quoicoubeh '
+    data = data.encode("utf8")
+    s.sendall(data)
+except ConnectionRefusedError :
+    print(" Connexion au serveur échouée ")
+
+finally :
+    s.close()
+
