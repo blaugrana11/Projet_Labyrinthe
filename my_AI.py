296aabb51aa88d93619acc5899463de88ea2ac9d
Nabil Hajchaib <21168@ecam.be>
2023-03-30 12:57:22 +0200

    last_change

--- my_AI.py
+++ my_AI.py
@@ -1,70 +1,58 @@
+import socket
 import json
 import time
-import socket
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host, port = ('localhost', 4000)
+server_address = ('localhost', 3000)
+Variable = True
+
+port = int(sys.argv[1])
+
+# Création de la requête de souscription
 request = {
-   "request": "subscribe",
-   "port": 8888,
-   "name": "fun_name_for_the_client",
-   "matricules": ["12345", "67890"]
+    "request": "subscribe",
+    "port": port,
+    "name": "Avoine-{}".format(port),
+    "matricules": ["21160", "20057", str(port)]
 }
 
-request_ping = {
-   "response": "pong"
-}
-
-'''request_coup = {
-   "response": "move",
-   "move": the_move_played,
-   "message": "Fun message"
-}'''
-
-surrender = {
-   "response": "giveup",
-}
-
-try :
-    s.connect((host, port))
-    print("Le client se connecte... ")
-    s.sendall(json.dumps(request).encode())
-    response = s.recv(2048).decode()
-    print(response)
-except ConnectionRefusedError :
-    print(" Connexion au serveur échouée ")
-
-finally :
-    s.close()
+# Création de la socket et envoi de la requête de souscription au serveur
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(5)
+    try:
+        s.connect(server_address)
+        s.sendall(json.dumps(request).encode())
+        response = s.recv(1024).decode()
+        print(response)
+    except socket.timeout:
+        print("Le temps d'attente pour la connexion est trop long !")
+        pass
 
 # Création de la socket et écoute sur le port de souscription
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('localhost', 8888))
+    s.bind(('', port))
     s.listen()
 
-    while True:
+    while Variable:
         s.settimeout(5)
             
         try: 
             # Acceptation de la connexion entrante
             client_socket, client_address = s.accept()
-            print('Connexion de', client_address)
+            with client_socket:
+                print('Connexion de', client_address)
 
-            # Réception du message envoyé par le serveur
-            data = client_socket.recv(1024).decode()
-            print('Reçu', repr(data))
+                # Réception du message envoyé par le serveur
+                data = client_socket.recv(16000).decode()
+                print('Reçu', repr(data))
 
-            # Analyse du message reçu et envoi de la réponse appropriée
-            message = json.loads(data)
-            if message['request'] == 'ping':
-                response = {"response": "pong"}
-                print(response)
-                client_socket.sendall(json.dumps(response).encode())
+                # Analyse du message reçu et envoi de la réponse appropriée
+                message = json.loads(data)
+                if message['request'] == 'ping':
+                    response = {"response": "pong"}
+                    print(response)
+                    client_socket.sendall(json.dumps(response).encode())
 
-            # Fermeture de la connexion
-            client_socket.close()
-        except:
-            print("Trop long pour l'accept")
+        except socket.timeout:
             pass
-
-        Variable = False #Pour arrêter la boucle étant donné qu'on est déja accepé
+print(type(message))
